adf4620e44bd0140823dd6b15999b51cb7ac19c2
Souhaila <sola.111me@gmail.com>
2019-02-03 05:22:52 +0200

    DALAndBLL

---
+++ BLL/PostsManager.cs
@@ -0,0 +1,34 @@
+﻿using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public static class PostsManager
+    {
+        static LinkedInEntitiesContext linkedInContext = 
+            new LinkedInEntitiesContext();
+        public static List<Post> GetAll()
+        {
+            return linkedInContext.Posts.ToList();
+        }
+        public static List<Post> GetAllByDate()
+        {
+            return linkedInContext.Posts
+                .OrderBy(p=>p.Date).ToList();
+        }
+        public static List<Post> GetAllByTop()
+        {
+            return linkedInContext.Posts
+                .OrderBy(p => p.Num_Of_Comments).ToList();
+        }
+        public static List<Post> GetAllByUserId(int userId)
+        {
+            return linkedInContext.Posts
+                .Where(e=>e.FK_CreatorId == userId).ToList();
+        }
+    }
+}
---
+++ Presentation/Home.aspx.cs
@@ -0,0 +1,25 @@
+﻿using BLL;
+using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Presentation
+{
+    public partial class Home : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                posts.DataSource = PostsManager.GetAll();
+                posts.DataTextField = "Post_Content";
+                posts.DataValueField = "id";
+                posts.DataBind();
+            }
+        }
+    }
+}
